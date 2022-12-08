a0bc39c0c78916b6cdcf3be90e9c1f0db9223dfb
been <emptyinteger@kakao.com>
2022-12-08 17:56:54 +0900

    ReservationServices save only works at orderlist status == ing

--- src/main/java/webProject/SIProject/service/ReservationServices.java
+++ src/main/java/webProject/SIProject/service/ReservationServices.java
@@ -18,8 +18,12 @@ public class ReservationServices {
 
     @Transactional
     public void save(String status, Reservation_DTO infoDto) {
+        if(status != "ing") {
+            return;
+        }
         OrderList orderList = orderRepository.findByStatus(status)
                 .orElseThrow(IllegalArgumentException::new);
+
         int len = infoDto.getSelected().length;
         int i;
         for(i = 0; i < len; i++) {
