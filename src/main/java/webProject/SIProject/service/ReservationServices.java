8b967cc410967900ad8bb1f5c4786a9e1311741d
been <emptyinteger@kakao.com>
2022-12-19 22:54:47 +0900

    comment removed

--- src/main/java/webProject/SIProject/service/ReservationServices.java
+++ src/main/java/webProject/SIProject/service/ReservationServices.java
@@ -100,7 +100,6 @@ public class ReservationServices {
         if (orderList.isPresent()) {
             OrderList ord = orderList.get();
             reservation = reservationRepository.findByOrderList(ord);
-            System.out.println("성공함");
         }
         return reservation;
     }
