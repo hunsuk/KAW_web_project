27e5f1c859328b4b2901b5864428b64355c040c3
hunsuk <daw916@naver.com>
2022-12-21 11:54:03 +0900

    최종

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,6 +6,8 @@ insert into pallet_item(category,front_type,size,weight,material,uniqueness,img_
 insert into pallet_item(category,front_type,size,weight,material,uniqueness,img_uri,standard) value("표준형","13B형","1,300mm X 1,100mm X 150mm","25.0kg","HDPE","적재하중 1Ton/4방 차입형 양면 사용형","http://ajnetworks.co.kr/assets/uploads/product/product_13B_1.png","B13");
 insert into pallet_item(category,front_type,size,weight,material,uniqueness,img_uri,standard) value("표준형","15A형","1,460mm X 1,130mm X 150mm","27.5kg","HDPE","적재하중 1Ton/4방 차입형 편면 사용형","http://ajnetworks.co.kr/assets/uploads/product/product_15A_1.png","A15");
 insert into pallet_item(category,front_type,size,weight,material,uniqueness,img_uri,standard) value("제지용","국판형","650mm X 950mm X 145mm","13.0kg","HDPE","적재하중 1Ton/4방 차입형 양면 사용형","http://ajnetworks.co.kr/assets/uploads/product/product_gookpan_1.png","국판");
+insert into pallet_item(category,front_type,size,weight,material,uniqueness,img_uri,standard) value("제지용","4*6판형","800mm X 1,100mm X 145mm","20.0kg","HDPE","적재하중 1Ton/4방 차입형 양면 사용형","http://ajnetworks.co.kr/assets/uploads/product/product_4x6_1.png","4x6판");
+insert into pallet_item(category,front_type,size,weight,material,uniqueness,img_uri,standard) value("제지용","국배판형","950mm X 1,280mm X 140mm","26.0kg","HDPE","적재하중 1Ton/4방 차입형 양면 사용형","http://ajnetworks.co.kr/assets/uploads/product/product_gookbaepan_1.png","국배판");
 
 --고정 유저
 insert into user(auth,corp_name,create_at,email,location,manager_name,modified_at,password,phone)value("ROLE_USER","삼성 물산","2022-12-17 09:21:59","daw916@naver.com","11-180","김현석","2022-12-17 09:21:59","$2a$10$v/NhV.Nsme1rsH9vXkIG6.yT7UXmmE9YwGidUlqbnQlh7KGEwhk4a","01021033096");
